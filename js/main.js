ac5be5bbd5bfb533156a57cd124fe6de0e6372fb
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-24 21:13:27 +0300

    240223 2113

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(() => {
-    const showWaH = (el) => {
-        const classElem = '.' + el
-        const w = $(classElem).width()
-        const h = $(classElem).height()
-        $('.mainText p').text('Ширина: ' + w + '. Высота: ' + h)
-    }
-
-    showWaH('arrowDown')
+$(document).ready(() => {
+    console.log($('.mainText').html())
 })
