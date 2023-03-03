2501a210166811b95860902104e48904574b6464
Evgeniy Salish <jeka5650962@gmail.com>
2023-03-03 09:53:56 +0300

    030323 0953

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(document).ready(() => {
-    function addAttribute(element, attribute, value) {
-        const getElement = '.' + element
-        $(getElement).attr(attribute, value)
-    }
-
-    addAttribute('logo', 'title', 'Daisy')
+    $('menu').addClass('border')
+    $('section.receive > div').removeClass('icons')
 })
